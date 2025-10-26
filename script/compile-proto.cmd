4dea3240c33889a8fb780fcda5eb051d6c7aac14
Simon Lin <xiaonanln@gmail.com>
2025-10-26 22:36:20 +1100

    Fix: Update Docker image name in compile-proto.cmd for protobuf compilation

--- script/compile-proto.cmd
+++ script/compile-proto.cmd
@@ -1,7 +1,7 @@
 @echo off
 REM Compile protobuf files using Docker
 echo Compiling protobuf files...
-docker run -it --rm -v %CD%:/app dev ./script/compile-proto.sh
+docker run -it --rm -v %CD%:/app goverse-dev ./script/compile-proto.sh
 if %ERRORLEVEL% equ 0 (
     echo Protobuf compilation completed successfully.
 ) else (
