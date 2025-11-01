8d5e00aa5264b839d482eafada18b56edd1fe271
Simon Lin <xiaonanln@gmail.com>
2025-11-01 23:54:11 +1100

    Add failfast option to Go unit test command for quicker feedback on errors

--- script/docker/test-go.sh
+++ script/docker/test-go.sh
@@ -28,7 +28,7 @@ echo "Running Go unit tests..."
 echo
 
 # run all go tests (no caching) and fail fast on errors
-if ! go test ./... -p 1 -count=1 -v; then
+if ! go test ./... -p 1 -count=1 -v -failfast; then
     echo "✗ Go unit tests failed"
     exit 1
 fi
