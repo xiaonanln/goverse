3c87e2fc9d343d4be5d6eb4e1607404d806c3d0e
Simon Lin <xiaonanln@gmail.com>
2025-11-02 23:56:57 +1100

    Add logging for CreateObject failures and initialize server logger

--- samples/chat/server/chat_server.go
+++ samples/chat/server/chat_server.go
@@ -3,9 +3,13 @@ package main
 import (
 	"context"
 	"flag"
+	"time"
 
 	"github.com/xiaonanln/goverse/goverseapi"
+	"github.com/xiaonanln/goverse/util/logger"
 )
+
+var serverLogger = logger.NewLogger("ChatServer")
 
 func main() {
 	var (
@@ -36,10 +40,13 @@ func initializeChatServer() {
 	goverseapi.RegisterClientType((*ChatClient)(nil))
 	goverseapi.RegisterObjectType((*ChatRoomMgr)(nil))
 	goverseapi.RegisterObjectType((*ChatRoom)(nil))
-	
+
 	// Create ChatRoomMgr0 when cluster is ready
 	go func() {
+		serverLogger.Infof("Waiting for cluster to be ready...")
 		<-goverseapi.ClusterReady()
+		serverLogger.Infof("Cluster is ready, creating ChatRoomMgr0 in 5 seconds...")
+		time.Sleep(5 * time.Second)
 		goverseapi.CreateObject(context.Background(), "ChatRoomMgr", "ChatRoomMgr0", nil)
 	}()
 }
